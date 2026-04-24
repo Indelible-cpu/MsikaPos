5287e3bd183fdf3ad44b2fb840446ca9b8cca8fa
James P Dickson <dicksonpetroj4@outlook.com>
2026-04-24 21:47:30 +0200

    fix: increase body size limit to 50mb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,8 +29,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('dev'));
 
 // Health Check
